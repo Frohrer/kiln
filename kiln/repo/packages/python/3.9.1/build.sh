82fd6f760392bc2b3f42ce6abf615c9782f27e0a
Frederic Rohrer <github@frohrer.com>
2025-04-11 21:31:38 -0400

    mhm fixes

--- kiln/repo/packages/python/3.9.1/build.sh
+++ kiln/repo/packages/python/3.9.1/build.sh
@@ -18,4 +18,4 @@ cd ..
 
 rm -rf build 
 
-pip3 install numpy pillow requests pandas matplotlib scipy scikit-learn flask django beautifulsoup4 boto3 botocore urllib3 grpcio-status aiobotocore certifi charset-normalizer setuptools s3fs idna s3transfer typing-extensions python-dateutil fsspec packaging google-api-core six pyyaml cryptography  whoosh bcrypt passlib sympy
+pip3 install numpy pillow requests pandas matplotlib scipy flask django beautifulsoup4 boto3 botocore urllib3 grpcio-status aiobotocore certifi charset-normalizer setuptools s3fs idna s3transfer typing-extensions python-dateutil fsspec packaging google-api-core six pyyaml cryptography  whoosh bcrypt passlib sympy
